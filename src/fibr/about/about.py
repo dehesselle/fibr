59d40657fa214991375a131cdd729ddae3331615
René de Hesselle <dehesselle@web.de>
2025-10-23 16:42:09 +0200

    Update about dialog

--- src/fibr/about/about.py
+++ src/fibr/about/about.py
@@ -1,3 +1,5 @@
+from importlib.resources import read_text
+
 from textual.app import ComposeResult
 from textual.binding import Binding
 from textual.screen import ModalScreen
@@ -5,24 +7,8 @@ from textual.widgets import TextArea, Footer
 
 import fibr.util as util
 
-
-# pyfiglet -f cricket fibr
-ABOUT_TEXT = """
-      ___ __ __
-    .'  _|__|  |--.----.          fibr [faɪbə]
-    |   _|  |  _  |   _|
-    |__| |__|_____|__|       dual-pane file browser
-
-
-  First line of text here! Max line size ----------->|
-
-
-
-  Last line of text here! Max line size ------------>|
-
-  (c) 2025 René de Hesselle.
-  Licensed under [TBD].
-"""
+# logo: pyfiglet -f cricket fibr
+ABOUT_TEXT = read_text("fibr.about", "about.txt")
 
 
 class AboutDialog(ModalScreen):
